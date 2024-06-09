c40d0b605167ffc292a5374bfda807c6aca6c9c1
Vladislav <111982932+v1ab1@users.noreply.github.com>
2024-06-09 20:44:42 +0300

    fix paths

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+// Utils
+export {cn} from './tools/cn'
+
+// Components
+export {Radio} from './Radio'
+export {Button, buttonVariants} from './Button'
+export {Toggle} from './Toggle'
+export {Checkbox} from './checkboxNew'
+export {TextArea} from './TextArea'
+export {Input} from './inputNew'
+export {Icon} from './Icon'
+export {SmallIcon} from './SmallIcon'
+export {IconButton} from './IconButton'
+export {Progress} from './Progress'
+export {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from './cardNew'
+export {Menu} from './Menu'
+export {Tab} from './Tab/Tab'
+export {List, ListContent, ListIcon, ListSubtitle, ListTitle, ListItem, ListActionIcon} from './List'
+export {Tabs} from './Tabs'
+export {InputFile} from './InputFile'
+export {CodeEditor} from './CodeEditor'
+export {Table, TableHeader, TableBody, TableFooter, TableHead, TableRow, TableCell, TableCaption} from './Table'
+export {Avatar, AvatarFallback, AvatarImage} from './avatar'
+export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from './dropdown-menu'
+export {Switch} from './switch'
+export {Popover, PopoverContent, PopoverTrigger} from './popover'
+export {RadioGroup, RadioGroupItem} from './radio-group'
+
+// Interfaces
+export type {ITab} from './Tab/Tab'
+
+// Types
+export type {Icon as IconType} from './Icon'
+
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+} from './Select'
